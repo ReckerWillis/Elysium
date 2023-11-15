4bb4206b2cea61e2c1ea13fa151c63a986d42145
Huseinmuffin04 <21523004@students.uii.ac.id>
2023-11-15 15:31:12 +0700

    Karakter Utama Jalan dan Lompat
    animasi Karakter utama berjalan dam lompat

---
+++ Assets/Script/Playercontroller.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontroller : MonoBehaviour
+{
+    [Header("Horizontal Movement Setting")]
+    [SerializeField]private float walkSpeed = 1;
+    
+    [Header("Ground Check Setting")]
+    [SerializeField]private float jumpForce = 45;
+    [SerializeField]private Transform groundCheckPoint;
+    [SerializeField]private float groundCheckY = 0.2f;
+    [SerializeField] private float groundCheckX = 0.5f;
+    [SerializeField] private LayerMask whatIsGround;
+
+    private float xAxis;
+    private Rigidbody2D rb;
+    Animator anim;
+
+    public static Playercontroller Instance;
+
+    private void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInputs();
+        Move();
+        Jump();
+        Flip();
+    }
+
+    void GetInputs()
+    {
+        xAxis = Input.GetAxisRaw("Horizontal");
+    }
+
+    void Flip()
+    {
+        if(xAxis < 0)
+        {
+            transform.localScale = new Vector2(-1,transform.localScale.y);
+        }
+        else if(xAxis > 0)
+        {
+            transform.localScale = new Vector2(1, transform.localScale.y);
+        }
+        
+
+        
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(walkSpeed * xAxis, rb.velocity.y);
+        anim.SetBool("Walking", rb.velocity.x != 0 && Grounded());
+    }
+    public bool Grounded()
+    {
+        if (Physics2D.Raycast(groundCheckPoint.position, Vector2.down, groundCheckY, whatIsGround)
+            || Physics2D.Raycast(groundCheckPoint.position + new Vector3(groundCheckX,0,0), Vector2.down, groundCheckY, whatIsGround)
+            || Physics2D.Raycast(groundCheckPoint.position + new Vector3(-groundCheckX, 0, 0), Vector2.down, groundCheckY, whatIsGround))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    void Jump()
+    {
+        if (Input.GetButtonUp("Jump")&& rb.velocity.y > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x,0);
+        }
+        if(Input.GetButtonDown("Jump")&& Grounded())
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce);
+        }
+
+        anim.SetBool("Jumping", !Grounded());
+
+
+    }
+
+}
