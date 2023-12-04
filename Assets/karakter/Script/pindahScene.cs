b75493a06527b876f38c0a6b1fd1085695a7a5d1
Muhammad Surya Ramadhan <suryaramadhan850@gmail.com>
2023-12-05 05:57:41 +0700

    title screen
    belum ada background buat title screen

---
+++ Assets/karakter/Script/pindahScene.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pindahScene : MonoBehaviour
+{
+    public void pindah (string tujuan){
+        SceneManager.LoadScene(tujuan);
+    }
+    
+}
